8ddc4880f5281399169ee7e054e41cc6204dec80
Mikael Fehrm <mikael.fehrm@1337.tech>
2023-04-06 11:28:04 +0200

    Init - Monorepo - Created Angular microfrontend 1, 2 and root/shell projects - Some routing for mf1

---
+++ projects/mf1/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component.js';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  pathMatch: 'full'
+}, {
+  path: 'items',
+  loadChildren: () => import('./items/items.module').then(m => m.ItemsModule)
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
